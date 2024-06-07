988472a6cb567f7da35b25a920fa2c756c258255
Ni9er120404 <Niger120404@yandex.ru>
2024-06-08 01:09:53 +0300

    Task was completed

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,12 @@
 package encoding
 
 import (
+	"encoding/json"
+	_ "fmt"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -25,16 +30,50 @@ type MyEncoder interface {
 
 // Encoding перекодирует файл из JSON в YAML
 func (j *JSONData) Encoding() error {
-	// ниже реализуйте метод
-	// ...
+	var yamlData YAMLData
+	fileInput, err := os.ReadFile(j.FileInput)
+	if err != nil {
+		panic("не удалось прочитать файл")
+	}
+	err = json.Unmarshal(fileInput, &yamlData.DockerCompose)
+	if err != nil {
+		panic("не удалось десериализировать")
+	}
 
+	fileOutput, err := os.Create(j.FileOutput)
+	if err != nil {
+		panic("не удалось создать файл")
+	}
+	defer fileOutput.Close()
+	bytesOfYaml, err := yaml.Marshal(yamlData.DockerCompose)
+	if err != nil {
+		panic("не удалось преобразовать файл в json")
+	}
+	fileOutput.Write(bytesOfYaml)
 	return nil
 }
 
 // Encoding перекодирует файл из YAML в JSON
 func (y *YAMLData) Encoding() error {
-	// Ниже реализуйте метод
-	// ...
+	var jsonData JSONData
+	fileInput, err := os.ReadFile(y.FileInput)
+	if err != nil {
+		panic("не удалось прочитать файл")
+	}
+	err = yaml.Unmarshal(fileInput, &jsonData.DockerCompose)
+	if err != nil {
+		panic("не удалось десериализировать")
+	}
 
+	fileOutput, err := os.Create(y.FileOutput)
+	if err != nil {
+		panic("не удалось создать файл")
+	}
+	defer fileOutput.Close()
+	bytesOfJson, err := json.Marshal(jsonData.DockerCompose)
+	if err != nil {
+		panic("не удалось преобразовать файл в json")
+	}
+	fileOutput.Write(bytesOfJson)
 	return nil
 }
